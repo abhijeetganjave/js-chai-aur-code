8cce5a836074ffacbfffdffd81e525097454a8ba
abhijeetganjave <144986910+abhijeetganjave@users.noreply.github.com>
2024-08-02 08:03:01 +0000

    Data_Types

---
+++ 01_Basics/02_Data_Types.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node.js not browser
+
+console.log(3 + 3);
+console.log("Abhijeet");
+
+let name = "Abhijeet"
+let age = 18
+let isLoggedIn = true
+
+console.log(typeof undefined);
+console.log(typeof null);
