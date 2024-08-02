68c6eda9c799928146de5cb0c111ae4c1d02f924
abhijeetganjave <144986910+abhijeetganjave@users.noreply.github.com>
2024-08-02 08:02:15 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "abhijeetganjave04@gmail.com"
+var accountPass = "123450"
+accountCity = "karad" // not good way , but it works
+
+let accountState; //if value is not given it is undefined
+
+/*
+
+prefer not to use var beacuse issue
+in block scope and functional scope
+
+never use var
+
+*/
+
+//accountId = 2
+accountEmail = "dbhsdnsand@cmdszmc.com"
+accountCity = "nndxc"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPass,accountState])
+
