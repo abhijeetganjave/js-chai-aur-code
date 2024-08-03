05c0c59ff7181b10894c9abb1fdea21da74313bf
abhijeetganjave <144986910+abhijeetganjave@users.noreply.github.com>
2024-08-03 13:04:51 +0000

    Strings

---
+++ 01_Basics/08_Strings.js
@@ -0,0 +1,43 @@
+let name = "Abhijeet"
+
+let name2 = "Ganjave"
+
+const repoCnt = 55
+
+console.log(name + name2);
+
+
+console.log(`Hello my name is ${name} and my repo cnt is ${repoCnt}`);
+
+
+let str = new String('Abhi-jeet')
+
+console.log(str.charAt(2));
+console.log(str.indexOf('t'));
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str[0]);
+console.log(str.__proto__);
+
+let str2 = str.substring(0,4)
+console.log(str2);
+
+let str3 = str.slice(-9,4)
+console.log(str3);
+
+
+let s1 = "      AbhiGanjave     "
+
+console.log(s1)
+console.log(s1.trim())
+
+const url = "https://fluffy-%36%enigma-v6v7j594jx45h74x.github.dev/"
+
+console.log(url.replace('%36%','-'));
+
+console.log(url.includes('fluffy'));
+
+
+let arr = ['abhi','ganjave','hp','victus']
+
+
